9564bd2e84a7b6723df287443051d87e1e04456a
Phoenix Nicholson <phoenix.nicholson2008@gmail.com>
2022-03-31 10:36:22 -0700

    wrote test for redirecting to oauth

---
+++ __tests__/github.test.js
@@ -0,0 +1,21 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('backend-gitty routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+  it('should be able to redirect to the github oauth page', async () => {
+    const req = await request(app).get('/api/v1/github/login');
+
+    expect(req.header.location).toMatch(
+      /https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&scope=user&redirect_uri=http:\/\/localhost:7890\/api\/v1\/github\/login\/callback/i
+    );
+  });
+});
