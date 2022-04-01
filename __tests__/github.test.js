6e6a22ed595da000cf963aa780ea90e8bf7ce3f6
Phoenix Nicholson <phoenix.nicholson2008@gmail.com>
2022-03-31 17:45:57 -0700

    fixing imports and names and all the things to get that test  to pass

--- __tests__/github.test.js
+++ __tests__/github.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+jest.mock('../lib/utils/github');
 
 describe('backend-gitty routes', () => {
   beforeEach(() => {
