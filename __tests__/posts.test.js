f55d6a14f6eac5ec7b0afdd3a136f10eaa665087
Phoenix Nicholson <phoenix.nicholson2008@gmail.com>
2022-04-01 11:54:46 -0700

    wrote test for get all posts

---
+++ __tests__/posts.test.js
@@ -0,0 +1,32 @@
+const setup = require('../data/setup');
+const app = require('../lib/app');
+const request = require('../lib/app');
+const pool = require('../lib/utils/pool');
+
+describe('backend-gitty routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+
+  it.only('should be able to get all posts', async () => {
+    const agent = request.agent(app);
+    await request(app).get('/api/v1/github/login');
+    await agent.get('/api/v1/github/login/callback?code=42').redirects(1);
+
+    const post1 = {
+      id: expect.any(String),
+      text: 'post one',
+    };
+    const post2 = {
+      id: expect.any(String),
+      text: 'post two',
+    };
+
+    const res = await agent.get('/api/v1/posts');
+    expect(res.body).toEqual([post1, post2]);
+  });
+});
