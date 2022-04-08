40e889bae8082d288540a0d59261fddd10b83202
Phoenix Nicholson <phoenix.nicholson2008@gmail.com>
2022-04-08 16:59:52 -0700

    adding imports

--- __tests__/ quote.test.js
+++ __tests__/ quote.test.js
@@ -1,4 +1,7 @@
-const { request } = require('../lib/app');
+const app = require('../lib/app');
+const request = require('supertest');
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
 
 describe('backend-gitty routes', () => {
   beforeEach(() => {
@@ -8,22 +11,22 @@ describe('backend-gitty routes', () => {
   afterAll(() => {
     pool.end();
   });
-});
-it('should get a list of quotes from an array of api', async () => {
-  const expected = [
-    {
-      author: expect.any(String),
-      content: expect.any(String),
-    },
-    {
-      author: expect.any(String),
-      content: expect.any(String),
-    },
-    {
-      author: expect.any(String),
-      content: expect.any(String),
-    },
-  ];
-  const res = await request(app).get('/api/v1/quotes');
-  expect(res.body).toEqual(expected);
+  it('should get a list of quotes from an array of api', async () => {
+    const expected = [
+      {
+        author: expect.any(String),
+        content: expect.any(String),
+      },
+      {
+        author: expect.any(String),
+        content: expect.any(String),
+      },
+      {
+        author: expect.any(String),
+        content: expect.any(String),
+      },
+    ];
+    const res = await request(app).get('/api/v1/quotes');
+    expect(res.body).toEqual(expected);
+  });
 });
