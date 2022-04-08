8e4261487f3b882031a66f5f9d1cfda65255fdd5
Phoenix Nicholson <phoenix.nicholson2008@gmail.com>
2022-04-08 16:30:13 -0700

    fixing nested .then

--- __tests__/github.test.js
+++ __tests__/github.test.js
@@ -33,7 +33,7 @@ describe('backend-gitty routes', () => {
   it('should be able to sign out a user', async () => {
     await GithubUser.insert({
       username: 'fake_github_user',
-      avatar_url: 'https://www.placecage.com/gif/300/300',
+      avatar: 'https://www.placecage.com/gif/300/300',
       email: 'not-real@example.com',
     });
 
